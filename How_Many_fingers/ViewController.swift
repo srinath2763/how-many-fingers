c45306723ed54411123edbfba0c6922457363487
IMCS2 <IMCS2@imcs1s-macbook-air-2.local>
2019-02-06 23:28:26 -0600

    Initial Commit

---
+++ How_Many_fingers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  How_Many_fingers
+//
+//  Created by IMCS2 on 2/6/19.
+//  Copyright © 2019 IMCS2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
